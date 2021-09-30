52c1838131e539eea95ad5ce6bd4e56d258b53de
Lam Son Ho <holamson2000@gmail.com>
2021-09-30 11:47:49 +0700

    update

--- src/sicp_in_clojure/ch3.clj
+++ src/sicp_in_clojure/ch3.clj
@@ -10,8 +10,35 @@
 (defn make-withdraw
   [bal]
   (let [balance (atom bal)]
-    (fn [amount] 
+    (fn [amount]
       (if (>= @balance amount)
         (swap! balance #(- % amount))
         "Insufficient funds"))))
 
+(defn make-account [bal]
+  (let
+    [balance (atom bal)
+     withdraw
+        (fn [amount]
+          (if (>= @balance amount)
+            (swap! balance #(- % amount))
+            "Insufficient funds"))
+     deposit
+        (fn [amount] (swap! balance #(+ % amount)))
+     dispatch
+        (fn [m]
+          (cond (= m 'withdraw) withdraw
+                (= m 'deposit) deposit
+                :else (throw (Exception. "Unknown request"))))]
+    dispatch))
+
+(comment
+(def acc1 (make-account 100))
+(def acc2 (make-account 100))
+((acc1 'withdraw) 20)
+((acc2 'withdraw) 10)
+((acc1 'withdraw) 30)
+((acc2 'deposit) 100)
+((acc1 'deposit) 50)
+((acc1 'depo) 10000000)
+)
--- src/sicp_in_clojure/ex3.clj
+++ src/sicp_in_clojure/ex3.clj
@@ -1,10 +1,9 @@
 (ns sicp-in-clojure.ex3
-  (:require [sicp-in-clojure.ch3] 
+  (:require [sicp-in-clojure.ch3]
             [sicp-in-clojure.ch1]
             ))
 
 ; --------------------------------------------------
-
 (defn make-accumulator
   "Exercise 3.1"
   [init]
@@ -23,30 +22,29 @@
 )
 
 ; --------------------------------------------------
-
 (defn make-monitored
   "Exercise 3.2"
   [fun]
   (let
     [counter (atom 0)
-     monitored 
-      (fn ([x] 
-            (cond 
+     monitored
+      (fn ([x]
+            (cond
               (= x 'how-many-calls?) @counter
               (= x 'reset-count) (reset! counter 0)
               :else
                 (do (swap! counter inc)
                     (fun x))))
-          ([x & xs] 
+          ([x & xs]
             (swap! counter inc)
             (apply fun x xs))
-          ([] 
+          ([]
             (swap! counter inc)
             (fun)))]
     monitored))
 
 (defn sqrt [x] (Math/sqrt x))
-(comment 
+(comment
 "Test code for Ex3.2"
 (def s (make-monitored sqrt))
 (= (s 'how-many-calls?) 0)
@@ -58,6 +56,85 @@
 (println (s 'how-many-calls?))
 (take 77 (map s (range)))
 (println (s 'how-many-calls?))
+)
+
+; --------------------------------------------------
+(defn make-account
+  "Exercise 3.3"
+  [bal password]
+  (let
+    [balance (atom bal)
+     withdraw
+        (fn [amount]
+          (if (>= @balance amount)
+            (swap! balance #(- % amount))
+            "Insufficient funds"))
+     deposit
+        (fn [amount] (swap! balance #(+ % amount)))
+     dispatch
+        (fn [pwd m]
+          (if (= pwd password)
+            (cond (= m 'withdraw) withdraw
+                  (= m 'deposit) deposit
+                  :else (throw (Exception. "Unknown request")))
+            (fn [& _] "Incorrect password")))]
+    dispatch))
+
+(comment
+(def acc1 (make-account 100 'ac1pass))
+(def acc2 (make-account 100 'ac2pass))
+((acc1 'ac1pass 'withdraw) 20)
+((acc2 'ac2pass 'withdraw) 10)
+((acc1 'wrongpwd 'withdraw) 30)
+((acc1 'ac1pass 'withdraw) 30)
+((acc2 'ac2pass 'deposit) 100)
+((acc1 'ac1pass 'deposit) 50)
+((acc1 'ac1pass 'depo) 10000000)
+((acc2 'wrongpwd 'withdraw) 1000)
+((acc2 'ac2pass 'withdraw) 1000)
+)
+
+; --------------------------------------------------
+(defn make-account
+  "Exercise 3.4"
+  [bal password]
+  (let
+    [
+      balance (atom bal)
+      num-tries (atom 0)
+      MAX-N-TRIES 7
+      ;------
+      withdraw
+        (fn [amount]
+          (if (>= @balance amount)
+            (swap! balance #(- % amount))
+            "Insufficient funds"))
+      deposit
+        (fn [amount] (swap! balance #(+ % amount)))
+      bad-password
+        (fn [& _]
+          (if (< @num-tries MAX-N-TRIES)
+              (do (swap! num-tries inc)
+                  "Incorrect password")
+              "Call the cops!"))
+      dispatch
+        (fn [pwd m]
+          (if (= pwd password)
+            (do (reset! num-tries 0)
+                (cond (= m 'withdraw) withdraw
+                      (= m 'deposit) deposit
+                      :else (throw (Exception. "Unknown request"))))
+            bad-password))
+    ]
+    dispatch))
+
+(comment
+(def acc1 (make-account 100 'pass1))
+(take 6 (map (acc1 'wrongpass 'withdraw) (repeat 1)))
+((acc1 'pass1 'withdraw) 10)
+(take 7 (map (acc1 'wrongpass 'withdraw) (repeat 1)))
+((acc1 'pass1 'withdraw) 10)
+(take 8 (map (acc1 'wrongpass 'withdraw) (repeat 1)))
 )
 
 ; --------------------------------------------------
