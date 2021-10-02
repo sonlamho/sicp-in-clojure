e796f378742b29b4cbc87f63d3c3bc17e3de91cd
Lam Son Ho <holamson2000@gmail.com>
2021-10-03 02:42:55 +0700

    test falsy? truthy?

--- test/sicp_in_clojure/utils_test.clj
+++ test/sicp_in_clojure/utils_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [sicp-in-clojure.utils :refer [all? falsy? truthy?]]))
 
+(deftest falsy?-test
+  (testing "falsy?"
+    (is (falsy? nil))
+    (is (falsy? false))
+    (is (not (falsy? true)))
+    (is (not (falsy? 1)))
+    (is (not (falsy? 0)))))
+
+(deftest truthy?-test
+  (testing "truthy? is opposite of falsy?"
+    (let [testvals [0 1 -1 true false nil "" "0" "1" :0 :nil :false :true]]
+      (is (= (into [] (map falsy? testvals))
+             (into [] (map #(not (truthy? %)) testvals)))))))
+
 (deftest all?-test
   (testing "all?"
     (is (truthy? (all? [1 true 10])))
