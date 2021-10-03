f2d64f05f961b3459ff5071ba4488c5735149b9b
Lam Son Ho <holamson2000@gmail.com>
2021-10-03 11:57:00 +0700

    add lein-cljfmt plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :plugins [[cider/cider-nrepl "0.25.3"]]
+  :plugins [[cider/cider-nrepl "0.25.3"]
+            [lein-cljfmt/lein-cljfmt "0.8.0"]]
   :main ^:skip-aot sicp-in-clojure.core
   :repl-options {:init-ns sicp-in-clojure.core}
   :profiles {:uberjar {:aot :all}})
